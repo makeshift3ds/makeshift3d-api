02c9e0fc3bb3ff5195bc2328a86c0406c0a2799c
Ken Elliott <makeshift3ds@gmail.com>
2017-12-13 16:01:29 -0500

    add routes for posts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const pageRoutes = require('./routes/pages');
+const postRoutes = require('./routes/posts');
 
 const app = express();
 
 app.use('/pages', pageRoutes);
+app.use('/posts', postRoutes);
 
 module.exports = app;
---
+++ src/routes/posts.js
@@ -0,0 +1,21 @@
+const Router = require('express-promise-router');
+const router = new Router();
+
+const db = require('../db');
+
+router.get('/', async (req, res) => {
+  const { rows } = await db.query('select * from posts', null);
+  res.send(result.rows);
+});
+
+router.get('/:slug', async (req, res) => {
+  const { slug } = req.params;
+  const { rows } = await db.query('select * from posts where slug = $1', [slug]) || {};
+  if (!rows.length) {
+    res.sendStatus(404);
+  } else {
+    res.send(rows[0]);
+  }
+});
+
+module.exports = router;
