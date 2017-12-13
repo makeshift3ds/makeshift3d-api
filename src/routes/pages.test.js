77145f96927e694553508e1403949569dda01bd5
Ken Elliott <makeshift3ds@gmail.com>
2017-12-13 16:19:26 -0500

    use async/await and jest instead of supertest

--- src/routes/pages.test.js
+++ src/routes/pages.test.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test the root path', () => {
-  test('It should response the GET method', () => {
-    return request(app).get('/pages').expect(200);
+describe('pages route', () => {
+  test('should work', async () => {
+    const response = await request(app).get('/pages');
+    expect(response.statusCode).toBe(200);
   });
 })
