26877de1a573e3ef33d0e72d380fc5ef164b9343
Ken Elliott <makeshift3ds@gmail.com>
2017-12-14 09:04:03 -0500

    enable cors for all origins

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const pageRoutes = require('./routes/pages');
 const postRoutes = require('./routes/posts');
 
 const app = express();
+app.use(cors());
 
 app.use('/pages', pageRoutes);
 app.use('/posts', postRoutes);
